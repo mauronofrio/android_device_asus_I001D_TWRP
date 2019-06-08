c42908b1782a5de8e603ff8fb011fb25fc4e66a9
mauronofrio matarrese <poronofrio@gmail.com>
2019-06-08 20:27:30 +0200

    Hi, I'm Asus ZenFon 6 I01WD

---
+++ README.md
@@ -0,0 +1,3 @@
+# Device Tree Asus ZenFone 6 I01WD
+
+Not tested
---
+++ device.mk
@@ -0,0 +1,39 @@
+
+LOCAL_PATH := device/asus/I01WD
+
+# define hardware platform
+PRODUCT_PLATFORM := msmnile
+
+#TEST
+# A/B support
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier
+
+PRODUCT_PACKAGES += \
+    bootctrl.msmnile
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Enable update engine sideloading by including the static version of the
+# boot_control HAL and its dependencies.
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.msmnile \
+    libgptutils \
+    libz \
+    libcutils
+
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+
+
+
