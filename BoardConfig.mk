64a57f0f2338addb4f3f1a65e73c7e17ddefde11
Captain Throwback <captainthrowback@hotmail.com>
2020-06-04 10:45:26 -0400

    I001D: remove TW_EXCLUDE_SUPERSU flag
    This has been removed upstream
    
    Change-Id: Ib545c04821f4c536011b81531ad2ab33f60f6de3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,6 @@ RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_EXCLUDE_SUPERSU := true
 TW_EXTRA_LANGUAGES := true
 TW_INCLUDE_NTFS_3G := true
 AB_OTA_UPDATER := true
